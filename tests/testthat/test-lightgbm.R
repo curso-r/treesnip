9ca2a81f715e48d0f4af84bac8636f4e5962e447
Daniel Falbel <dfalbel@gmail.com>
2021-07-09 20:35:37 -0300

    add a simple lightgbm test

--- tests/testthat/test-lightgbm.R
+++ tests/testthat/test-lightgbm.R
@@ -6,19 +6,19 @@
 # })
 #
 #
-# test_that('lightgbm alternate objective', {
-#   skip_if_not_installed("lightgbm")
-#
-#   spec <- boost_tree(mtry = 1, trees = 50, tree_depth = 15, min_n = 1) %>%
-#     set_engine("lightgbm", objective = "huber") %>%
-#     set_mode("regression")
-#
-#   lgb_fit <- spec %>% fit(mpg ~ ., data = mtcars)
-#
-#   info <- jsonlite::fromJSON(lightgbm::lgb.dump(lgb_fit$fit))
-#
-#   expect_equal(info$objective, "huber")
-# })
+test_that('lightgbm alternate objective', {
+  skip_if_not_installed("lightgbm")
+
+  spec <- boost_tree(mtry = 1, trees = 50, tree_depth = 15, min_n = 1) %>%
+    set_engine("lightgbm", objective = "huber") %>%
+    set_mode("regression")
+
+  lgb_fit <- spec %>% fit(mpg ~ ., data = mtcars)
+
+  info <- jsonlite::fromJSON(lightgbm::lgb.dump(lgb_fit$fit))
+
+  expect_equal(info$objective, "huber")
+})
 #
 # test_that("lightgbm with tune", {
 #
