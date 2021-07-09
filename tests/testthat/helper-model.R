abaac2ef2e95521ca8ef8c838ff3e5d1627f8f63
Daniel Falbel <dfalbel@gmail.com>
2021-07-09 18:52:02 -0300

    use larger sample to fix error from catboost.

--- tests/testthat/helper-model.R
+++ tests/testthat/helper-model.R
@@ -93,7 +93,7 @@ expect_categorical_vars_works <- function(model) {
 
 expect_can_tune_boost_tree <- function(model) {
 
-  mtcars <- rbind(mtcars, mtcars)
+  mtcars <- dplyr::sample_n(mtcars, size = 500, replace = TRUE)
 
   mtcars$cyl <- factor(mtcars$cyl)
   mtcars$vs <- factor(mtcars$vs)
