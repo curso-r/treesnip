28640fdee9f4325e72968f116957b6a985f8ccb8
Daniel Falbel <dfalbel@gmail.com>
2021-07-09 21:48:30 -0300

    enable different test

--- tests/testthat/test-lightgbm.R
+++ tests/testthat/test-lightgbm.R
@@ -1,24 +1,24 @@
-# test_that("lightgbm", {
-#
-#   model <- parsnip::boost_tree(mtry = 1, trees = 50, tree_depth = 15, min_n = 1)
-#
-#   expect_all_modes_works(model, 'lightgbm')
-# })
-#
-#
-test_that('lightgbm alternate objective', {
-  skip_if_not_installed("lightgbm")
+test_that("lightgbm", {
 
-  spec <- boost_tree(mtry = 1, trees = 50, tree_depth = 15, min_n = 1) %>%
-    set_engine("lightgbm", objective = "huber") %>%
-    set_mode("regression")
+  model <- parsnip::boost_tree(mtry = 1, trees = 50, tree_depth = 15, min_n = 1)
 
-  lgb_fit <- spec %>% fit(mpg ~ ., data = mtcars)
-
-  info <- jsonlite::fromJSON(lightgbm::lgb.dump(lgb_fit$fit))
-
-  expect_equal(info$objective, "huber")
+  expect_all_modes_works(model, 'lightgbm')
 })
+#
+#
+# test_that('lightgbm alternate objective', {
+#   skip_if_not_installed("lightgbm")
+#
+#   spec <- boost_tree(mtry = 1, trees = 50, tree_depth = 15, min_n = 1) %>%
+#     set_engine("lightgbm", objective = "huber") %>%
+#     set_mode("regression")
+#
+#   lgb_fit <- spec %>% fit(mpg ~ ., data = mtcars)
+#
+#   info <- jsonlite::fromJSON(lightgbm::lgb.dump(lgb_fit$fit))
+#
+#   expect_equal(info$objective, "huber")
+# })
 #
 # test_that("lightgbm with tune", {
 #
